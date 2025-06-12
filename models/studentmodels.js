c5c8db1ef4e77a0aee762e886d2beba13fcbbe80
Hamdi-H <hamdi.hussen134@gmail.com>
2025-06-12 12:12:11 +0300

    first commit towards the creation of API

---
+++ models/studentmodels.js
@@ -0,0 +1,20 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');  //this is how you import in node js
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema ({
+    firstname:{
+        type:String,
+        required:[true,'firstname is required']
+    },
+    lastname:{
+        type:String,
+        required:[true,'lastname is required']
+    },
+    gender:{
+        type:String
+    }
+});
+
+const student = mongoose.model('student',studentSchema);
+module.exports=student;
